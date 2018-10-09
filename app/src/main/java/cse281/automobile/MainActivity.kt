7a856860aa83d4f41a7b3e0205091639067e8866
Luke Bernick <lmb420@lehigh.edu>
2018-10-08 20:20:59 -0400

    Doesn't compile. Need to sync

--- app/src/main/java/cse281/automobile/MainActivity.kt
+++ app/src/main/java/cse281/automobile/MainActivity.kt
@@ -1,30 +1,216 @@
 package cse281.automobile
 
+import android.Manifest
+import android.app.Fragment
+import android.content.Context
+import android.content.pm.PackageManager
+import android.graphics.Color
+import android.hardware.camera2.CameraAccessException
+import android.hardware.camera2.CameraCharacteristics
+import android.hardware.camera2.CameraManager
+import android.hardware.camera2.params.StreamConfigurationMap
+import android.media.Image
+import android.media.ImageReader
+import android.os.Build
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Trace
+import android.util.Size
+import android.widget.Toast
+import cse281.automobile.R.id.textView
 import kotlinx.android.synthetic.main.activity_main.*
+import cse281.automobile.CameraConnectionFragment
+import java.util.logging.Logger
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), ImageReader.OnImageAvailableListener
+{
+    private val PERMISSIONS_REQUEST = 1
+
+    private val PERMISSION_CAMERA = Manifest.permission.CAMERA
+    private val PERMISSION_STORAGE = Manifest.permission.WRITE_EXTERNAL_STORAGE
+
+    private var useCamera2API: Boolean = false
+    private var isProcessingFrame = false
+    private val yuvBytes = arrayOfNulls<ByteArray>(3)
+    private var rgbBytes: IntArray? = null
+
+    protected var previewWidth = 0
+    protected var previewHeight = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-    // Example of a call to a native method
-    sample_text.text = stringFromJNI()
+        if (hasPermission()) {
+            setFragment()
+        } else {
+            requestPermission()
+        }
+
+        // Example of a call to a native method
+        textView.setBackgroundColor(Color.parseColor("#ffffff"))
     }
 
     /**
-     * A native method that is implemented by the 'native-lib' native library,
-     * which is packaged with this application.
+     * Callback for Camera2 API
      */
-    external fun stringFromJNI(): String
+    override fun onImageAvailable(reader: ImageReader) {
+        //We need wait until we have some size from onPreviewSizeChosen
+        if (previewWidth == 0 || previewHeight == 0) {
+            return
+        }
+        if (rgbBytes == null) {
+            rgbBytes = IntArray(previewWidth * previewHeight)
+        }
+        try {
+            val image = reader.acquireLatestImage() ?: return
 
-    companion object {
+            if (isProcessingFrame) {
+                image.close()
+                return
+            }
+            isProcessingFrame = true
+            Trace.beginSection("imageAvailable")
+            val planes = image.planes
+            fillBytes(planes, yuvBytes)
+            yRowStride = planes[0].rowStride
+            val uvRowStride = planes[1].rowStride
+            val uvPixelStride = planes[1].pixelStride
 
-        // Used to load the 'native-lib' library on application startup.
-        init {
-            System.loadLibrary("native-lib")
+            imageConverter = Runnable {
+                ImageUtils.convertYUV420ToARGB8888(
+                        yuvBytes[0],
+                        yuvBytes[1],
+                        yuvBytes[2],
+                        previewWidth,
+                        previewHeight,
+                        yRowStride,
+                        uvRowStride,
+                        uvPixelStride,
+                        rgbBytes)
+            }
+
+            postInferenceCallback = Runnable {
+                image.close()
+                isProcessingFrame = false
+            }
+
+            processImage()
+        } catch (e: Exception) {
+            LOGGER.e(e, "Exception!")
+            Trace.endSection()
+            return
+        }
+
+        Trace.endSection()
+    }
+
+    override fun onRequestPermissionsResult(
+            requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        if (requestCode == PERMISSIONS_REQUEST) {
+            if (grantResults.isNotEmpty()
+                    && grantResults[0] == PackageManager.PERMISSION_GRANTED
+                    && grantResults[1] == PackageManager.PERMISSION_GRANTED) {
+                setFragment()
+            } else {
+                requestPermission()
+            }
+        }
+    }
+
+    private fun hasPermission() : Boolean {
+        return checkSelfPermission(PERMISSION_CAMERA) == PackageManager.PERMISSION_GRANTED &&
+                checkSelfPermission(PERMISSION_STORAGE) == PackageManager.PERMISSION_GRANTED;
+    }
+
+    private fun requestPermission() {
+        if (shouldShowRequestPermissionRationale(PERMISSION_CAMERA) || shouldShowRequestPermissionRationale(PERMISSION_STORAGE)) {
+            Toast.makeText(this@MainActivity,
+                    "Camera AND storage permission are required for this app", Toast.LENGTH_LONG).show()
+        }
+        requestPermissions(arrayOf(PERMISSION_CAMERA, PERMISSION_STORAGE), PERMISSIONS_REQUEST)
+    }
+
+    // Returns true if the device supports the required hardware level, or better.
+    private fun isHardwareLevelSupported(
+            characteristics: CameraCharacteristics, requiredLevel: Int): Boolean {
+        val deviceLevel = characteristics.get(CameraCharacteristics.INFO_SUPPORTED_HARDWARE_LEVEL)!!
+        return if (deviceLevel == CameraCharacteristics.INFO_SUPPORTED_HARDWARE_LEVEL_LEGACY) {
+            requiredLevel == deviceLevel
+        } else requiredLevel <= deviceLevel
+        // deviceLevel is not LEGACY, can use numerical sort
+    }
+
+    private fun chooseCamera(): String? {
+        val manager = getSystemService(Context.CAMERA_SERVICE) as CameraManager
+        try {
+            for (cameraId in manager.cameraIdList) {
+                val characteristics = manager.getCameraCharacteristics(cameraId)
+
+                // We don't use a front facing camera in this sample.
+                val facing = characteristics.get(CameraCharacteristics.LENS_FACING)
+                if (facing != null && facing == CameraCharacteristics.LENS_FACING_FRONT) {
+                    continue
+                }
+
+                characteristics.get(CameraCharacteristics.SCALER_STREAM_CONFIGURATION_MAP)
+                        ?: continue
+
+// Fallback to camera1 API for internal cameras that don't have full support.
+                // This should help with legacy situations where using the camera2 API causes
+                // distorted or otherwise broken previews.
+                useCamera2API = facing == CameraCharacteristics.LENS_FACING_EXTERNAL || isHardwareLevelSupported(characteristics,
+                        CameraCharacteristics.INFO_SUPPORTED_HARDWARE_LEVEL_FULL)
+                return cameraId
+            }
+        } catch (e: CameraAccessException) {
+
+        }
+
+        return null
+    }
+
+    protected fun setFragment() {
+        val cameraId = chooseCamera()
+        if (cameraId == null) {
+            Toast.makeText(this, "No Camera Detected", Toast.LENGTH_SHORT).show()
+            finish()
+        }
+
+        val fragment: Fragment
+        if (useCamera2API) {
+            val camera2Fragment = CameraConnectionFragment.newInstance(
+                    object : CameraConnectionFragment.ConnectionCallback() {
+                         override fun onPreviewSizeChosen(size: Size, rotation: Int) {
+                            previewHeight = size.height
+                            previewWidth = size.width
+                            this@CameraActivity.onPreviewSizeChosen(size, rotation)
+                        }
+                    },
+                    this,
+                    getLayoutId(),
+                    getDesiredPreviewFrameSize())
+
+            camera2Fragment.setCamera(cameraId)
+            fragment = camera2Fragment
+        }
+
+        fragmentManager
+                .beginTransaction()
+                .replace(R.id.container, fragment)
+                .commit()
+
+    }
+
+    protected fun fillBytes(planes: Array<Image.Plane>, yuvBytes: Array<ByteArray>) {
+        // Because of the variable row stride it's not possible to know in
+        // advance the actual necessary dimensions of the yuv planes.
+        for (i in planes.indices) {
+            val buffer = planes[i].buffer
+            if (yuvBytes[i] == null) {
+                yuvBytes[i] = ByteArray(buffer.capacity())
+            }
+            buffer.get(yuvBytes[i])
         }
     }
 }
